johnnychen94/jill.py

commit 04785ed5ed8d4524dc06f7d901426a8f4f4f93ae

download and verify gpg signature

jill/download.py
@@ -2,6 +2,7 @@ from .source import SourceRegistry
 from .version_utils import latest_version
 from .version_utils import is_version_released
 from .sys_utils import current_system, current_architecture
+from .gpg_utils import verify_gpg
 
 import wget
 import os
@@ -16,7 +17,7 @@ from typing import Optional
 from urllib.error import URLError
 
 
-def _download_package(url: str, out: str):
+def _download(url: str, out: str):
     # always do overwrite
     outpath = os.path.abspath(out)
     outdir, outname = os.path.split(outpath)
@@ -96,7 +97,41 @@ def download_package(version=None, sys=None, arch=None, *,
     outname = os.path.split(urlparse(url).path)[1]
     outpath = os.path.join(outdir, outname)
 
-    if os.path.isfile(outpath) and not overwrite:
+    if (os.path.isfile(outpath) and os.path.isfile(outpath+".asc")
+            and not overwrite):
         logging.info(f"{outname} already exists, skip downloading")
         return outpath
-    return _download_package(url, outpath)
+
+    package_path = _download(url, outpath)
+
+    if system in ["windows", "macos"]:
+        # macOS and Windows releases are codesigned with certificates
+        # that are verified by the operating system during installation
+        return package_path
+    elif system in ["linux", "freebsd"]:
+        # additional verification using GPG
+        if not package_path:
+            return package_path
+
+        # a mirror should provides both *.tar.gz and *.tar.gz.asc
+        gpg_signature_path = _download(url+".asc", outpath+".asc")
+        if not gpg_signature_path:
+            msg = f"failed to download GPG signature for {release_str}"
+            logging.warning(msg)
+            logging.info(f"remove untrusted/broken file")
+            os.remove(package_path)
+            return False
+
+        if not verify_gpg(package_path, gpg_signature_path):
+            logging.warning(f"failed to verify {release_str} downloads")
+            msg = f"remove untrusted/broken files"
+            logging.info(msg)
+            os.remove(package_path)
+            os.remove(gpg_signature_path)
+            return False
+
+        # GPG-verified julia release path
+        logging.info(f"success to verify {release_str} downloads")
+        return package_path
+    else:
+        raise ValueError(f"unsupported system {sys}")

MANIFEST.in
@@ -1,1 +1,2 @@
 include jill/config/*
+include jill/.gnupg/*

jill/.gnupg/juliareleases.asc
@@ -1,0 +1,52 @@
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v1
+
+mQINBFXxFlcBEADQDEBFlzoyehPuk13Ct928WwBvb0q9OKyjz2NlYq3sL5ReTbQB
+9P5hyl68q5iJ6QTjKEaxr+Kmjhib9dQGZhtBXRa9q185Fdav48rS9rDKR5/aPXNi
+4aA0BSp7fHIDrTUGOUMB5TFpVZil+Sz4llpPKDlgG70dn3ZLBznJQKUXJWhxrheG
+ogUK4W3WAdBBPDVraPjBjvTTSrhoOBJh/oNib3J6xTIaUMhOFz+Vuq05BZI9UO6n
+OsE3dSW7X7dvqjcN3Ti7TgbJD5d4iOsQl8NhqItyS8ZULV8TPGOuwitoWxqgFIAL
+5bhM9Of4xOE0+rmgke1dKmMkq3cu6yCEFypqyxwShexe+1Mvx4Tn4/OqC7wFVpTA
+IH2ys7NsVcoLtZGqlBQnbXFmIu9ay51Zb4wwbJ5Qr9Rfx5xPvJoOVUpP/0I8+vlI
+CmBkP6vs9vMCCKcreP0FpjCTSRApv9IXuwjumOMb6P0GJPOuFVfsy4849ONPC/yM
+dMbeopi/BWfHu/Nqt7pqY210jncsdBPlPy7LvvhIkbpeZHQDoQVDPX88ZylhqKTy
+gpWPBT5ezJ5ib0nSvYIZjMOMlMWxDaNDBGZlyHizVFwLZk6qHWM7I2WbJGvNgBTv
+0dX9jBIDhdKdSZjc3wxh+nqZQg1l8xOOx9yCLSiBL1OHf4PYqJudL09AUwARAQAB
+tDNKdWxpYSAoQmluYXJ5IHNpZ25pbmcga2V5KSA8YnVpbGRib3RAanVsaWFsYW5n
+Lm9yZz6JAjgEEwECACIFAlXxFlcCGwMGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheA
+AAoJEGbjx9wD1uSVg78QAJZUeygDHj1zTxt+8UAm4TMu0nWmcPjSzTGj5Wt4Gtec
+HlWsXTOvFbABv8r3vzD2W1Bi0D0UcUucBy3Jf0nrUBWY89VTREcG/EWsF2SwSB7H
+cL3pu+vcdLiVtRGI4AiSoZz2CXc4vHY0X/3TlPejcO0UU8A0Ukth/cX1ZqCjKP8T
+ciXy89X4mlRAsAXapkHxiO+bscTd/VdWaPaUx8/TxeFoPZFB/0FIeJHYbI1chKPd
+vAtFYLpB89d8zbQYgISM6oc/f1j0CQR6JdHGoAGP9Wd8wRz+mDT3WzOqL4jXctcA
+CQUKGgYkOW8OEFBlfUACZK5uFxWMktN8//IlzczCTbYb9Z89UeeF7oaXfSZMFwiF
+kxseUGCceXb5Kqj3fZKmmUstAEzycyNuCeXG1KXyAz1mg/ihq/rzB11vQQjY4WYJ
+rIoUecRN3btSex6jcdOxAIOeGcyfigT7NMgplFXXkbuux2N7qtOkLUNx80DMOggK
+tnSP60GkO1xzJLi3EHtaDVPU59KpeXjyEsNB2ngc5+LwHwbYGvaaZaFXFm7oCmM7
+xG88EU14mCLZbpGleD6cmpVAprFSIXV0Z0xm6pdH9XBCT4UJ8tFXTrJsc1dYd+mw
+eAwCYZ38e95kqrYrRbhjOOAKEtf3t4VnrsifbTfTVclUbsrSXVTQdHoiMlODc/WX
+uQINBFXxFlcBEADNmFCh53NJ+8CQSzQda/efBX+H/SCj2b3vIYJXY2nR9h4IQ7UV
+/AU5sUB/bpIN3nwwdcILYSm2oJGP8fZ8Zf46XliUOK8+yD8ApDg6okl3R1G+E9Qk
+/EN49BCeXx9uT5vHpcHWkBvKmqmjUJ283i6q3QT5qzbkCGGUQ7SyhU1ywbjYIQi/
+HLJpntqz44LrM+vfGUAa+CJld3DyzAm66KFSRbDU12XPE948MxUDQ1NgY9hJIlfm
+ud/ShKakfQoEsLiTkUbEY7Vc19s2+aM3S1zeRfsatuayPuEUsnuz42wKWSdPNGyJ
+TkLdWz46vSgN9wpe0OLoWxsuomaViRaNFDSK7Uo+AGjWcjFNlehFlW/ELji1JbS5
+f5EAD1A1I2RJvLHyri3xFJtM9qbGiA3ZIfcVXq5RxAOehDPCcKzBS4w37D2vLBOQ
+Xa+ExTJxwiCnMPuo7acsfkyleakAe82L/fAoVWdPcFSjq3KFvkpGpTlvvh2jwhoW
+AgDGu77K9T1rHjj7t2GjuR71RVc4r0CP9iF3rAPmq/FapONW1Pz0aom7XLBZt8Zq
+4wsPsGaAECmwi07bE6Vr9nqCeQb7XmjVucVJP+VXDpOJzt4J5zSzTCWGyj47/K7a
+Rlz9KtYmY0s4sKnx3sjKpC8xMXaLgvSjudrQCZ/sohKRayKGAMI2p71GbQARAQAB
+iQIfBBgBAgAJBQJV8RZXAhsMAAoJEGbjx9wD1uSV6+oP/3MCyMWEBiu73HVI2dS2
+hDct/E9fDkpB6o/HEGhdNFTeeb/L7GqcQACJDtBDNVtMu0WhCgKeteHXM0KMy55f
+6HAQEVnWhGSyR4KksV93RPZvUO+zzX5M7F2LiI59MSruKAYTC0kXbjcu9aQAn+kJ
+EPHiHwsTzRkWh90q54/B2NQ6oVAHgnMIeh32OBdFMNHOnP+n1zu/+Wd4miC3fR9V
+tmsVrOS8WtozdEC6TmquYswQ/gT6c0afCZSlNF/ZPPrXGGdD6t9WTJntfYB1rbEk
+E/9WpaUgpKpxXQEOMzMAm+2yBoYnCpXzvbY6fzNWfOg6DJ65t0rkrCwDRHLH1grA
+61OQb0Ou8LQnrFGox8L394sFebIoaBUk2Vhw5LH78X6g1f7Mj6j9Er0YSabVVpHh
+ncMYflOeswrV4C1oP5UvL7K3qtCixUU4LQ4XqmioQey8AnrCdJ7S5QeyP1n5vU3e
+Nz1JHCcH4/e698CuIoCZa86Edmo3S0O2hhiC5qslf5u1pdndlmbrgsWpBH5kJ7mI
+edeA2ND/KrLlllE7NImLdlrciShctFP1ciqqHtTebQ+5MH17ObOhSptUDEt5LjZt
+3YXZtQ+C/UmfkC+QVUdWTQ4cWUCNtuzLP+PW3o1AQHmijWbaECq5yMRVlr7JuxPr
+Lr+fAJHZvbYCQjMTkZYScgYU
+=XN/B
+-----END PGP PUBLIC KEY BLOCK-----

jill/defaults.py
@@ -24,7 +24,7 @@ def get_configfiles(filename):
 
 SOURCE_CONFIGFILE = get_configfiles("sources.json")
 RELEASE_CONFIGFILE = os.path.join(PKG_ROOT, "config", "releases.csv")
-
+GPG_PUBLIC_KEY_PATH = os.path.join(PKG_ROOT, ".gnupg", "juliareleases.asc")
 
 default_filename_template = "julia-$patch_version-$osarch.$extension"
 default_latest_filename_template = "julia-latest-$osbit.$extension"

jill/gpg_utils.py
@@ -1,0 +1,26 @@
+from .defaults import GPG_PUBLIC_KEY_PATH
+
+from gnupg import GPG
+from tempfile import TemporaryDirectory
+
+import os
+
+
+def _verify_gpg(gpg: GPG, datafile, signature_file):
+    with open(signature_file, "rb") as fh:
+        return gpg.verify_file(fh, data_filename=datafile)
+
+
+def verify_gpg(datafile, signature_file=None) -> bool:
+    """
+    verify Julia releases using GPG
+    """
+    if signature_file is None:
+        signature_file = datafile + ".asc"
+
+    with open(GPG_PUBLIC_KEY_PATH) as fh:
+        keycontent = fh.read()
+    with TemporaryDirectory() as tmpdir:
+        gpg = GPG(gnupghome=tmpdir)
+        gpg.import_keys(keycontent)
+        return bool(_verify_gpg(gpg, datafile, signature_file))

setup.py
@@ -16,7 +16,7 @@ with io_open(requirements, mode='r') as fd:
 
 setuptools.setup(
     name='jill',
-    version='0.2.2',
+    version='0.2.3',
     author="Johnny Chen",
     author_email="johnnychen94@hotmail.com",
     description="Julia Installer 4 Linux(and MacOS) - Light",

commit c23ece215c0ba3beb9861bb54fffdaae64b82f1f

fix skip_download for mac system
It was working but is broken by 04785ed5ed8d4524dc06f7d901426a8f4f4f93ae

jill/download.py
@@ -104,8 +104,15 @@ def download_package(version=None, sys=None, arch=None, *,
     outname = os.path.split(urlparse(url).path)[1]
     outpath = os.path.join(outdir, outname)
 
-    if (os.path.isfile(outpath) and os.path.isfile(outpath+".asc")
-            and not overwrite):
+    skip_download = False
+    if system in ["linux", "freebsd"]:
+        if (os.path.isfile(outpath) and os.path.isfile(outpath+".asc")
+                and not overwrite):
+            skip_download = True
+    else:
+        if os.path.isfile(outpath) and not overwrite:
+            skip_download = True
+    if skip_download:
         logging.info(f"{outname} already exists, skip downloading")
         return outpath
 
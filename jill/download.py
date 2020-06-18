johnnychen94/jill.py

commit d168764cb8dd6c728466cfcc76bbc7585db1665c

disable <0.6.0 version

jill/download.py
@@ -100,7 +100,7 @@ def download_package(version=None, sys=None, arch=None, *,
       max_try:
         try `max_try` times before returning a False. The default value is 3.
     """
-    version = str(version) if version else ""
+    version = str(version) if str(version) else ""
     version = "latest" if version == "nightly" else version
     version = "" if version == "stable" else version
 
@@ -118,10 +118,10 @@ def download_package(version=None, sys=None, arch=None, *,
         wrong_args = True
     if wrong_args:
         msg = f"something wrong for the platform argument you passed:\n"
-        msg += f"  - version: {version}\n"
+        msg += f"  - version(>= 0.6.0): {version}\n"
         msg += f"  - system: {system}\n"
         msg += f"  - archtecture: {architecture}\n"
-        msg += f"example: jill download 1 linux x86_64"
+        msg += f"example: `jill download 1 linux x86_64`"
         raise(ValueError(msg))
 
     release_str = f"{version}-{system}-{architecture}"

jill/install.py
@@ -312,7 +312,7 @@ def install_julia(version=None, *,
     install_dir = install_dir if install_dir else default_install_dir()
     symlink_dir = symlink_dir if symlink_dir else default_symlink_dir()
     system, arch = current_system(), current_architecture()
-    version = str(version) if version else ''
+    version = str(version) if str(version) else ''
     version = "latest" if version == "nightly" else version
     version = "" if version == "stable" else version
 
@@ -339,8 +339,8 @@ def install_julia(version=None, *,
         # hide the nested error stack :P
         wrong_args = True
     if wrong_args:
-        msg = f"wrong version argument: {version}\n"
-        msg += f"Example: jill install 1"
+        msg = f"wrong version(>= 0.6.0) argument: {version}\n"
+        msg += f"Example: `jill install 1`"
         raise(ValueError(msg))
 
     overwrite = True if version == "latest" else False

jill/utils/version_utils.py
@@ -220,11 +220,15 @@ def latest_major_version(version, system, architecture, **kwargs) -> str:
     return latest_patch
 
 
-def latest_version(version, system, architecture, update=True, **kwargs) -> str:
+def latest_version(version: str, system, architecture, update=True, **kwargs) -> str:
     """
     find the latest version for partial semantic version string. Directly
     return `version` if it's already a complete version string.
     """
+    # supporting legacy versions is really of low priority
+    if Version(version) < Version("0.6.0"):
+        raise(ValueError('Julia < v"0.6.0" is not supported.'))
+
     # if user passes a complete version here, then we don't need to query
     # from local storage, just trying to download it would be fine.
     if is_full_version(version):
@@ -307,7 +311,7 @@ def update_releases(system=None, architecture=None, *,
     }
     for _ in range(2):
         versions = set(map(lambda x: x[0], read_releases(stable_only=True)))
-        versions = versions if len(versions) != 0 else {"1.0.0"}
+        versions = versions if len(versions) != 0 else {"0.6.0"}
 
         for item in product(versions, systems, architectures):
             # get all x.y.0 versions

commit 0d9efcbd67525b95184f4ab832c3377aea86370f

fix a version bug introduced by d168764

jill/download.py
@@ -100,7 +100,7 @@ def download_package(version=None, sys=None, arch=None, *,
       max_try:
         try `max_try` times before returning a False. The default value is 3.
     """
-    version = str(version) if str(version) else ""
+    version = str(version) if (version or str(version) == "0") else ""
     version = "latest" if version == "nightly" else version
     version = "" if version == "stable" else version
 

jill/install.py
@@ -312,7 +312,7 @@ def install_julia(version=None, *,
     install_dir = install_dir if install_dir else default_install_dir()
     symlink_dir = symlink_dir if symlink_dir else default_symlink_dir()
     system, arch = current_system(), current_architecture()
-    version = str(version) if str(version) else ''
+    version = str(version) if (version or str(version) == "0") else ''
     version = "latest" if version == "nightly" else version
     version = "" if version == "stable" else version
 

jill/utils/version_utils.py
@@ -226,7 +226,7 @@ def latest_version(version: str, system, architecture, update=True, **kwargs) ->
     return `version` if it's already a complete version string.
     """
     # supporting legacy versions is really of low priority
-    if Version(version) < Version("0.6.0"):
+    if version and Version(version) < Version("0.6.0"):
         raise(ValueError('Julia < v"0.6.0" is not supported.'))
 
     # if user passes a complete version here, then we don't need to query
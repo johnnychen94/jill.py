d42d6e87fc760df7b02f4a3af96433a0913eb47a
Johnny Chen <johnnychen94@hotmail.com>
2020-08-30 21:28:05 +0800

    jill v0.7.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with io_open(requirements, mode='r') as fd:
 
 setuptools.setup(
     name='jill',
-    version='0.7.2',
+    version='0.7.3',
     author="Johnny Chen",
     author_email="johnnychen94@hotmail.com",
     description="JILL -- Julia Installer for Linux (MacOS, Windows and FreeBSD) -- Light",

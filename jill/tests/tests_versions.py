117dfaedf733e5590b804d54c3da7309fa788412
Johnny Chen <johnnychen94@hotmail.com>
2021-04-21 17:21:24 +0800

    fix tests

--- jill/tests/tests_versions.py
+++ jill/tests/tests_versions.py
@@ -6,52 +6,50 @@ import unittest
 
 class TestVersions(unittest.TestCase):
     def test_is_version_released(self):
-        self.assertTrue(is_version_released("1.1.0", "windows", "x86_64"))
+        self.assertTrue(is_version_released("1.1.0", "winnt", "x86_64"))
         self.assertFalse(is_version_released("1.1.0", "whatever", "x86_64"))
 
-        self.assertTrue(is_version_released("1.1.1", "macos", "x86_64"))
-        self.assertTrue(is_version_released("1.6.0", "windows", "x86_64"))
+        self.assertTrue(is_version_released("1.1.1", "mac", "x86_64"))
+        self.assertTrue(is_version_released("1.6.0", "winnt", "x86_64"))
         self.assertTrue(is_version_released("1.6.0", "linux", "x86_64"))
-        
-        self.assertFalse(is_version_released("1.5.10", "windows", "x86_64"))
 
-        self.assertTrue(is_version_released("0.1.2", "macos", "x86_64"))
-        self.assertFalse(is_version_released("0.1.2", "windows", "x86_64"))
-        self.assertFalse(is_version_released("0.1.2", "macos", "i686"))
+        self.assertFalse(is_version_released("1.5.10", "winnt", "x86_64"))
 
-        self.assertTrue(is_version_released("latest", "macos", "x86_64"))
+        self.assertTrue(is_version_released("0.1.2", "mac", "x86_64"))
+        self.assertFalse(is_version_released("0.1.2", "winnt", "x86_64"))
+        self.assertFalse(is_version_released("0.1.2", "mac", "i686"))
 
-        self.assertTrue(is_version_released("1.6.0-rc1", "macos", "x86_64", stable_only=False))
-        self.assertFalse(is_version_released("1.6.0-rc1", "macos", "x86_64", stable_only=True))
+        self.assertTrue(is_version_released("latest", "mac", "x86_64"))
+
+        self.assertTrue(is_version_released(
+            "1.6.0-rc1", "mac", "x86_64", stable_only=False))
+        self.assertFalse(is_version_released(
+            "1.6.0-rc1", "mac", "x86_64", stable_only=True))
 
     def test_latest_version(self):
         self.assertEqual(
-            latest_version("latest", "windows", "x86_64"),
+            latest_version("latest", "winnt", "x86_64"),
             "latest")
         self.assertEqual(
-            latest_version("0.7", "windows", "x86_64"),
+            latest_version("0.7", "winnt", "x86_64"),
             "0.7.0")
         self.assertEqual(
-            latest_version("1.1", "windows", "x86_64"),
+            latest_version("1.1", "winnt", "x86_64"),
             "1.1.1")
         self.assertEqual(
-            latest_version("1.5", "macos", "x86_64"),
+            latest_version("1.5", "mac", "x86_64"),
             "1.5.4")
-        self.assertEqual(
-            latest_version("1.8", "macos", "x86_64"),
-            "1.6.0")
         self.assertNotEqual(
-            latest_version("", "macos", "x86_64"),
+            latest_version("", "mac", "x86_64"),
             "latest")
+        self.assertRaises(ValueError, latest_version,
+                          "", "illegal_system", "x86_64")
         self.assertEqual(
-            latest_version("", "illegal_system", "x86_64"),
-            latest_version("", "linux", "x86_64"))
-        self.assertEqual(
-            latest_version("1.4.0-rc1", "windows", "x86_64"),
+            latest_version("1.4.0-rc1", "winnt", "x86_64"),
             "1.4.0-rc1")
         self.assertEqual(
-            latest_version("999.999.999", "windows", "x86_64"),
+            latest_version("999.999.999", "winnt", "x86_64"),
             "999.999.999")
         self.assertEqual(
-            latest_version("1.6", "linux", "ARMv7"),
+            latest_version("1.6", "linux", "armv7l"),
             "1.4.1")

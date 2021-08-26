5ef63934c8d28926cd8c3ea589fc040c4de58310
Johnny Chen <johnnychen94@hotmail.com>
2021-08-26 14:06:19 +0800

    word tweaks, display DEV-version and also list 'julia'

--- jill/list.py
+++ jill/list.py
@@ -1,4 +1,4 @@
-from .utils.defaults import default_install_dir, default_symlink_dir
+from .utils.defaults import default_symlink_dir
 from .utils import color
 from .install import get_exec_version
 
@@ -7,7 +7,7 @@ import os
 import subprocess
 
 
-def search_files(dir, pattern='julia-'):
+def search_files(dir, pattern):
     files = os.listdir(dir)
     julias = [x for x in files if re.match(pattern, x)]
     return julias
@@ -15,19 +15,19 @@ def search_files(dir, pattern='julia-'):
 
 def get_julia_build(path):
     ver_cmd = [path, "--startup=no",
-               "-e", "println(Base.GIT_VERSION_INFO.commit_short)"]
+               "-e", "println(VERSION, ',', Base.GIT_VERSION_INFO.commit_short)"]
     try:
         build = subprocess.check_output(ver_cmd).decode("utf-8")
-        build = build.lower().strip()
+        ver, build = build.lower().split(',')
+        return ver.strip(), build.strip()
     except:
-        build = ""
-    return build
+        return "", ""
 
 
 def get_binversion(path):
     binversion = get_exec_version(path)
     if binversion.endswith('dev'):
-        build = get_julia_build(path)
+        binversion, build = get_julia_build(path)
     else:
         # we want quick response and we don't really care the commit version of a stable release
         build = ''
@@ -46,9 +46,9 @@ def list_julia(*, symlink_dir=None):
     if not os.path.exists(symlink_dir):
         raise(ValueError(f"symlink dir {symlink_dir} doesn't exist!"))
 
-    julias = sorted(search_files(symlink_dir, pattern='julia-'), reverse=True)  # nopep8
+    julias = sorted(search_files(symlink_dir, pattern='^julia'), reverse=True)  # nopep8
     version_list = [get_binversion(os.path.join(symlink_dir, x)) for x in julias]  # nopep8
-    print(f"Find {len(julias)} executables from {color.UNDERLINE}{symlink_dir}{color.END}:")  # nopep8
+    print(f"Found {len(julias)} julia(s) in {color.UNDERLINE}{symlink_dir}{color.END}:")  # nopep8
     for binname, (binversion, build) in zip(julias, version_list):
         build_msg = f"+{build}" if build else ""
         binversion_msg = f"{color.RED}Invalid{color.END}" if binversion == '0.0.1' else binversion

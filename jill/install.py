38a732bebcade87a3c318bc06a2d4b21be642887
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-12-18 15:44:14 -0500

    Simplify functions for finding installed executables

--- jill/install.py
+++ jill/install.py
@@ -15,42 +15,6 @@ import shutil
 import subprocess
 
 
-def _get_installation_dir(preferred_versions=None, only_preferred=False):
-    """
-    Return the path to a Julia installation. If `preferred_versions` is `None`, then the latest
-    stable version is preferred. Otherwise, `preferred_versions` should be a list of versions, which
-    are probed in order.  If the path to a preferred version is found, then it is returned.  If no
-    preferred version is found and `only_preferred` is `False`, then the first path to an installed
-    Julia found is returned.  Otherwise, `None` is returned.
-
-    The preferred versions, if supplied, should have the form `major.minor`, or be `latest`. For
-    example, `preferred_versions = ['1.7', 'latest']. If `preferred_versions` is `None`, there is a
-    bit of latency while the latest stable version is fetched from a server, unless another jill.py
-    function has already fetched and cached it.
-    """
-    julias_root = default_install_dir()
-    if not os.path.isdir(julias_root):
-        return None
-
-    if preferred_versions is None:
-        stable_version = latest_version("", current_system(), current_architecture()) # e.g. '1.7.0'
-        stable_version  = '.'.join(stable_version.split('.')[0:2]) # e.g. '1.7'
-        preferred_versions = [stable_version]
-    preferred_julia_installations = ["julia-" + x for x in preferred_versions]
-    installed_julias = [x for x in os.listdir(julias_root ) if x.startswith("julia")]
-    found_installation = None
-    for preferred_installation in preferred_julia_installations:
-        if preferred_installation in installed_julias:
-            found_installation = preferred_installation
-            break
-    if found_installation:
-        return os.path.join(julias_root, found_installation)
-    elif installed_julias and not only_preferred:  # Else take the first installation, if there.
-        return os.path.join(julias_root, installed_julias[0])
-    else:  # Else there are no julia installations
-        return None
-
-
 def _get_relative_bin_path():
     system = current_system()
     if system == "winnt":
@@ -63,24 +27,18 @@ def _get_relative_bin_path():
         raise ValueError(f"Unsupported system {system}")
 
 
-def get_installed_bin_path(preferred_versions=None, only_preferred=False):
+def get_installed_bin_paths():
     """
-    Return the path to a Julia installation. If `preferred_versions` is `None`, then the latest
-    stable version is preferred. Otherwise, `preferred_versions` should be a list of versions, which
-    are probed in order.  If the path to a preferred version is found, then it is returned.  If no
-    preferred version is found and `only_preferred` is `False`, then the first path to an installed
-    Julia executable found is returned.  Otherwise, `None` is returned.
-
-    The preferred versions, if supplied, should have the form `major.minor`, or be `latest`. For
-    example, `preferred_versions = ['1.7', 'latest']. If `preferred_versions` is `None`, there is a
-    bit of latency while the latest stable version is fetched from a server, unless another jill.py
-    function has already fetched and cached it.
+    Return a dict whose keys are Julia version strings and whose values are
+    absolute paths to the corresponding Julia exectuable. Only jill.py's
+    default directory for Julia installations is searched.
+    The version strings either equal to `latest` or have the form `major.minor`.
     """
-    installation_path = _get_installation_dir(preferred_versions=preferred_versions, only_preferred=only_preferred)
-    if installation_path:
-        return os.path.join(installation_path, _get_relative_bin_path())
-    else:
+    julias_root = default_install_dir()
+    if not os.path.isdir(julias_root):
         return None
+    rel_bin_path = _get_relative_bin_path()
+    return {x.split("-")[1] : os.path.join(julias_root, x, rel_bin_path) for x in os.listdir(julias_root ) if x.startswith("julia")}
 
 
 def is_installed(version, check_symlinks=True):
